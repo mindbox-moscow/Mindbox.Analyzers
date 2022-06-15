4d018712d2ee1e2e18dcca0ea5f31c082d2b64d1
SivkovSavely <sivkov@mindbox.ru>
2022-06-15 12:51:37 +0500

    Add separate project for console app tests

--- Mindbox.Analyzers.PragmaSettingConsoleApplication/DiagnosticPragmaIgnoreAdder.cs
+++ Mindbox.Analyzers.PragmaSettingConsoleApplication/DiagnosticPragmaIgnoreAdder.cs
@@ -1,23 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Microsoft.CodeAnalysis;
 
 namespace MindboxAnalyzers;
 
 public class DiagnosticPragmaIgnoreAdder
 {
-    private readonly string[] _diagnosticIDsToSuppress;
+    private readonly string[]? _diagnosticIDsToSuppress;
 
     private const string PragmaWarningDisablePrefix = "#pragma warning disable Mindbox";
     private const string PragmaWarningRestorePrefix = "#pragma warning restore Mindbox";
 
-    public DiagnosticPragmaIgnoreAdder(string[] diagnosticIDsToSuppress = null)
+    public DiagnosticPragmaIgnoreAdder(string[]? diagnosticIDsToSuppress = null)
     {
         _diagnosticIDsToSuppress = diagnosticIDsToSuppress;
     }
 
-    public Dictionary<string, string> AddPragmasToCode(IEnumerable<Diagnostic> diagnostics)
+    public Dictionary<string, string> AddPragmasToCode(IEnumerable<Diagnostic>? diagnostics)
     {
         if (diagnostics is null) return new Dictionary<string, string>();
         
@@ -48,13 +45,14 @@ public class DiagnosticPragmaIgnoreAdder
             var sourceCode = sourceTree.GetText().ToString();
 
             var filename = sourceTree.FilePath;
-            var filects = (changedFiles.TryGetValue(filename, out var _filects) ? _filects : sourceCode).Split('\n').ToList();
+            var fileContents = (changedFiles.TryGetValue(filename, out var changedFileContents) ? changedFileContents : sourceCode)
+                .Split('\n').ToList();
 
             if (!insertionShifts.ContainsKey(filename)) insertionShifts[filename] = 0;
             
-            filects.Insert(insertionShifts[filename] + lineStart, $"#pragma warning disable {descriptor}");
-            filects.Insert(insertionShifts[filename] + lineEnd + 2, $"#pragma warning restore {descriptor}");
-            changedFiles[filename] = string.Join("\n", filects);
+            fileContents.Insert(insertionShifts[filename] + lineStart, $"#pragma warning disable {descriptor}");
+            fileContents.Insert(insertionShifts[filename] + lineEnd + 2, $"#pragma warning restore {descriptor}");
+            changedFiles[filename] = string.Join("\n", fileContents);
 
             insertionShifts[filename] += 2; // pragma disable and pragma restore lines
         }
@@ -63,22 +61,22 @@ public class DiagnosticPragmaIgnoreAdder
         foreach (var changedFile in changedFiles)
         {
             var filename = changedFile.Key;
-            var filects = changedFile.Value.Split('\n').ToList();
+            var fileContents = changedFile.Value.Split('\n').ToList();
             
-            for (var i = 0; i < filects.Count; i++)
+            for (var i = 0; i < fileContents.Count; i++)
             {
-                if (!filects[i].StartsWith(PragmaWarningRestorePrefix)) continue;
+                if (!fileContents[i].StartsWith(PragmaWarningRestorePrefix)) continue;
                 
-                var diagnosticId = filects[i].Substring(PragmaWarningRestorePrefix.Length);
+                var diagnosticId = fileContents[i].Substring(PragmaWarningRestorePrefix.Length);
                 
-                if (filects.Count < i + 1 || filects[i + 1] != PragmaWarningDisablePrefix + diagnosticId) continue;
+                if (fileContents.Count < i + 1 || fileContents[i + 1] != PragmaWarningDisablePrefix + diagnosticId) continue;
                     
-                filects.RemoveAt(i);
-                filects.RemoveAt(i);
+                fileContents.RemoveAt(i);
+                fileContents.RemoveAt(i);
                 i++;
             }
             
-            changedFiles[filename] = string.Join("\n", filects);
+            changedFiles[filename] = string.Join("\n", fileContents);
         }
 
         return changedFiles;
